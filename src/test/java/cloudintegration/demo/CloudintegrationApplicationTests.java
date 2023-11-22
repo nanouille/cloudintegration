eef9ccbd2a2969ffd1b95c523cfe4e9e33e959a4
nanouille <73785178+nanouille@users.noreply.github.com>
2023-11-22 12:28:54 +0100

    create item, see web

---
+++ src/test/java/cloudintegration/demo/CloudintegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package cloudintegration.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudintegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
